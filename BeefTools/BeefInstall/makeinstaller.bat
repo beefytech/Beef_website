a1e516d81f61a6cccbe34f2b9eaeb8f57c2b6ff2
Brian Fiete <bfiete@gmail.com>
2020-10-07 14:02:34 -0700

    Version bump

--- BeefTools/BeefInstall/makeinstaller.bat
+++ BeefTools/BeefInstall/makeinstaller.bat
@@ -1,7 +1,7 @@
 @SETLOCAL EnableDelayedExpansion
 @SET SRCDIR=..\..\..\Beef
-@SET CURVER=0.42.7
-@SET DESTNAME=BeefSetup_0_42_7.exe
+@SET CURVER=0.42.8
+@SET DESTNAME=BeefSetup_0_42_8.exe
 
 PUSHD %~dp0
 
--- BeefTools/BeefInstall/makenightly.bat
+++ BeefTools/BeefInstall/makenightly.bat
@@ -8,8 +8,8 @@ for /f "tokens=1-4 delims=/ " %%i in ("%date%") do (
 )
 
 @SET SRCDIR=..\..\..\Beef
-@SET CURVER=0.42.7 (Nightly %month%/%day%/%year%)
-@SET DESTNAME=BeefSetup_0_42_7__%month%_%day%_%year%.exe
+@SET CURVER=0.42.8 (Nightly %month%/%day%/%year%)
+@SET DESTNAME=BeefSetup_0_42_8__%month%_%day%_%year%.exe
 
 PUSHD %~dp0
 
