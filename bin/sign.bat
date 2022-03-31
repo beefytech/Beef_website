3f5252783e77b8bbbb0670919cf637e960c27db9
Brian Fiete <bfiete@gmail.com>
2022-03-31 06:52:26 -0700

    Sign_local changes

--- bin/sign.bat
+++ bin/sign.bat
@@ -1,9 +1,7 @@
 @ECHO OFF
-PUSHD %~dp0
-if EXIST "sign_local.bat" (
-	sign_local.bat %1
+if EXIST "%~dp0\sign_local.bat" (
+	"%~dp0\sign_local.bat" %1
 )
-POPD
 
 @SETLOCAL EnableDelayedExpansion
 @SET BINPATH=C:\Program Files (x86)\Windows Kits\10\bin\10.0.17763.0\x64
